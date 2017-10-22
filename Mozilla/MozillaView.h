fbd5789cb507e9e2507654d7dbdf341d91072e5a
Stefan Arentz <stefan@arentz.ca>
2017-10-22 14:49:55 -0400

    Initial Commit

---
+++ Mozilla/MozillaView.h
@@ -0,0 +1,13 @@
+//
+//  MozillaView.h
+//  Mozilla
+//
+//  Created by Stefan Arentz on 2017-10-22.
+//  Copyright © 2017 Stefan Arentz. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MozillaView : ScreenSaverView
+
+@end
---
+++ Mozilla/MozillaView.m
@@ -0,0 +1,52 @@
+//
+//  MozillaView.m
+//  Mozilla
+//
+//  Created by Stefan Arentz on 2017-10-22.
+//  Copyright © 2017 Stefan Arentz. All rights reserved.
+//
+
+#import "MozillaView.h"
+
+@implementation MozillaView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
